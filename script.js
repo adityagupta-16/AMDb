74955e80576fcfd087c27f4da0d51b198301dcb4
Aditya Gupta <dudeadi156@gmail.com>
2022-04-06 08:47:42 +0530

    Create script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const API_KEY = 'api_key=01a2bc2d2846e346aaf5ac2cf14b94bb';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
